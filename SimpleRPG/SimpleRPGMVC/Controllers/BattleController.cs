05f482102419956022a0d982a2e8c91c48176838
CKYong <ckyong@sogyo.nl>
2017-06-22 00:10:51 +0200

    Fixed dependency error. Simply adding

--- SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
+++ SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
@@ -1,8 +1,6 @@
-// ReSharper disable RedundantUsingDirective
 using Microsoft.AspNetCore.Mvc;
 using SimpleRPG;
 
-//todo fix namespaces in controller use dll
 namespace SimpleRPGMVC.Controllers
 {
     public class BattleController : Controller
