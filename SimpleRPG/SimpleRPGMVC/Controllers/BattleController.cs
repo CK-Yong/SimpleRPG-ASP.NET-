7b11020218df9c3c1f71e8936b0c6ba5fb315b64
CKYong <ckyong@sogyo.nl>
2017-06-21 21:35:14 +0200

    added dll, linked cs files work, but need to find out how to import a dll

--- SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
+++ SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
@@ -1,19 +1,15 @@
 // ReSharper disable RedundantUsingDirective
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SimpleRPG;
-//todo fix namespaces in controller
+
+//todo fix namespaces in controller use dll
 namespace SimpleRPGMVC.Controllers
 {
     public class BattleController : Controller
     {
         public IActionResult Index()
         {
+            var player = new Player("CK Yong");
             
-            Player player = new Player("CK Yong");
             return View();
         }
     }
