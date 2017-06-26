3347a14bb9739ae9a5266ec0c5e7fafbf9468f67
CKYong <ckyoung@sogyo.nl>
2017-06-26 10:00:36 +0200

    Started Cookie passing

--- SimpleRPG/SimpleRPG/Player.cs
+++ SimpleRPG/SimpleRPG/Player.cs
@@ -2,7 +2,7 @@
 {
     public sealed class Player : Entity
     {
-        public Player(string name, int attack = 5, int defense = 5, int life = 100, int mana = 100, int stamina = 100,
+        public Player(string name = "Zero", int attack = 5, int defense = 5, int life = 100, int mana = 100, int stamina = 100,
             int experience = 0)
         {
             Name = name;
--- SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
+++ SimpleRPG/SimpleRPGMVC/Controllers/BattleController.cs
@@ -6,20 +6,33 @@ using SimpleRPG.MonsterFactory;
 
 namespace SimpleRPGMVC.Controllers
 {
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Newtonsoft.Json;
+
     public class BattleController : Controller
     {
-        private EnemyFactory enemyFactory = new EnemyFactory();
-        private Battle battle;
-        private Player player;
+        private readonly EnemyFactory enemyFactory = new EnemyFactory();
 
         public IActionResult Index()
         {
-            //todo make json representation of objects.
-            player = new Player("CK Yong");
-            var enemy = enemyFactory.MakeEnemy("Rodent");
-            battle = new Battle(player, enemy);
+            if (!Request.Cookies.ContainsKey("battle"))
+            {
+                var player = new Player("CK Yong");
+                var enemy = enemyFactory.MakeEnemy("Rodent");
+                var battle = new Battle(player, enemy);
+                var serializedBattle = JsonConvert.SerializeObject(battle);
+                Response.Cookies.Append("battle", serializedBattle);
+            }
+            else
+            {
+                //todo: return battle as cookie?
+            }
 
             return View();
+        }
+
+        public IActionResult Attack()
+        {
         }
     }
 }
